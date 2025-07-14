4528864cba4ffe5d818c89c5f76d096e46720a68
Maryam Taliani <120661346+MaryTaliani@users.noreply.github.com>
2025-07-14 09:48:53 +0200

    Pagina Login creata

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
+
+  hearts = Array.from({ length: 30 });
+
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      this.authService.performLogin(this.loginForm.value);
+    }
+  }
+
+  generateStyle() {
+  return {
+    '--random-left': Math.random().toString(),
+    '--random-speed': Math.random().toString(),
+    '--random-size': (Math.random() * 1.5 + 1).toString(),
+    '--random-blur': (Math.random() * 4).toFixed(1) + 'px'   // 0 – 3px
+  } as any;
+}
 
 }
---
+++ src/app/services/auth.ts
@@ -0,0 +1,77 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  private tokenKey = 'auth_token';
+  private roleKey = 'auth_role';
+  loggedIn = signal(false);
+
+    public syncFromStorage() {
+    this.loggedIn.set(!!localStorage.getItem(this.tokenKey));
+  }
+
+    constructor(private http: HttpClient, private router: Router) {
+    this.syncFromStorage();
+  }
+
+    login(credentials: { username: string; password: string }) {
+      console.log('Sending credentials to backend:', credentials);
+      return this.http.post<any>('http://localhost:8080/api/v1/sessions/login', credentials);
+    }
+
+  performLogin(credentials: { username: string; password: string }): void {
+    this.login(credentials).subscribe({
+      next: (response) => {
+        if (response.success) {
+          localStorage.setItem('auth_token', response.sessiondata.token);
+          localStorage.setItem('userId', response.sessiondata.userId); 
+          localStorage.setItem('username', response.sessiondata.username);   
+
+          this.syncFromStorage();
+          
+          this.router.navigate(['/dashboard']); 
+
+        } else {
+          alert('Login failed: ' + response.message);
+        }
+      },
+      error: (err) => {
+        alert('Server error during login.');
+        console.error(err);
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem('userId');
+    localStorage.removeItem('username'); 
+    this.syncFromStorage();
+    this.router.navigate(['/login']);
+  }
+
+  getLoggedUser(): any | null {
+  return { 
+    token: this.getToken(),
+    userId: this.getUserId(),
+    username: localStorage.getItem('username')
+  };
+}
+  isLoggedIn(): boolean {
+    return this.loggedIn();
+  }
+  
+  getUserId(): number {
+    return +localStorage.getItem('userId')!;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+}
