b3e8a46340803126aac1f00d1d237c024066a9bc
Sabrina Cinque <cinque.sabrina@gmail.com>
2025-07-15 12:24:29 +0200

    aggiunto environments in gitignor

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'dateappback-production.up.railway.app/api', // SOSTITUISCI CON URL REALE
-  appName: 'Dating App',
-  jwtTokenKey: 'dating_app_token',
-  debug: false
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
-  appName: 'Dating App - DEV',
-  jwtTokenKey: 'dating_app_token',
-  debug: true
-};
