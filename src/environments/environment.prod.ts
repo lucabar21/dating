0001d0d5c456d729bd6cdb05503a600562a97146
lucabar21 <luca_barbuto@yahoo.it>
2025-07-15 10:47:00 +0200

    fix gitIgnore per environments

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://your-backend-url.com/api',
-  appName: 'Dating App',
-  jwtTokenKey: 'dating_app_token',
-  debug: false,
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'https://your-backend-url.com/api',
-  appName: 'Dating App - DEV',
-  jwtTokenKey: 'dating_app_token',
-  debug: true,
-};
